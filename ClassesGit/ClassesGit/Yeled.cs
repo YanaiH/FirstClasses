81f67b5d62aed3153ad8c8ec120a20f051486346
Yanai <yhyh0203@gmail.com>
2019-05-07 20:28:07 +0300

    first draft

---
+++ ClassesGit/ClassesGit/Yeled.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Yeled
+{
+    private string adjective;
+    private int age;
+    
+
+    //constructor
+    public Yeled(string adjective, int age)
+    {
+        this.adjective = adjective.ToLower();
+        if (age<18)
+        {
+            this.age = age;
+        }
+        else
+        {
+            Console.WriteLine("Not a Yeled, actually Zaken");
+            this.age = int.MaxValue;
+        }
+    }
+
+    public string Adjective()
+    {
+        return this.adjective;
+    }
+}
